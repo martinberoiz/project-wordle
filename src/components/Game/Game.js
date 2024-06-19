0b9cd0d74bda3977b7ec6344333c821c602834a5
Martin Beroiz <martinberoiz@gmail.com>
2024-06-19 11:00:53 -0700

    Exercise 1 finished

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +9,33 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  [guess, setGuess] = React.useState({ guess: "" });
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (guess.guess.length < 5) {
+          return;
+        }
+        console.log(guess);
+        setGuess({ guess: "" });
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        type="text"
+        value={guess.guess}
+        id="guess-input"
+        onChange={(e) => {
+          if (e.target.value.length > 5) {
+            return;
+          }
+          setGuess({ guess: e.target.value.toUpperCase() });
+        }}
+      />
+    </form>
+  );
 }
 
 export default Game;
