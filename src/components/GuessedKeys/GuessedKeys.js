22e16127de799b7b84528c39d22bcbb96661ae60
Martin Beroiz <martinberoiz@gmail.com>
2024-06-19 21:16:29 -0700

    Clean-up

--- src/components/GuessedKeys/GuessedKeys.js
+++ src/components/GuessedKeys/GuessedKeys.js
@@ -2,24 +2,33 @@ import React from "react";
 import { checkGuess } from "../../game-helpers";
 
 function GuessedKeys({ guessedWords: words, answer }) {
+  // All unrepeated keys guessed so far Set(["A", "V", "P", ...])
   const guessedKeys = new Set(words.map(({ word }) => word.split("")).flat());
+  // These are the 3 rows of the keyboard
   keyboardUp = "QWERTYUIOP";
   keyboardMiddle = "ASDFGHJKL";
   keyboardDown = "ZXCVBNM";
-  keyboard = [keyboardUp, keyboardMiddle, keyboardDown];
-  const alphabet = keyboard.map((w) => w.split("")).flat();
+  // All the possible individual letters
+  alphabet = [
+    ...keyboardUp.split(""),
+    ...keyboardMiddle.split(""),
+    ...keyboardDown.split(""),
+  ];
+  // The status of every letter (initialized as unknown)
   const keyStatus = Object.fromEntries(
     alphabet.map((letter) => {
       return [letter, "unknown"];
     })
   );
+  // Check the status of the guessed keys
   const keycheck = checkGuess([...guessedKeys].join(""), answer) || [{}];
+  // Update all the letters based on the new info about guessed keys
   for ({ letter, status: state } of keycheck) {
     keyStatus[letter] = state;
   }
   return (
     <div className="keyboard-wrapper">
-      {keyboard.map((row, index) => {
+      {[keyboardUp, keyboardMiddle, keyboardDown].map((row, index) => {
         return (
           <div className="keyboard-row" key={index}>
             {row.split("").map((key) => {
